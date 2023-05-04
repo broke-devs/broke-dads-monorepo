44c7b2f8d11785f77a208c869b99794813257bc1
Auroiah Morgan <amwebdev@outlook.com>
2023-05-03 22:37:28 -0400

    Add Tailwindcss

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,5 +1,9 @@
 import * as React from "react";
-
+import "./styles.css";
 export const Button = () => {
-  return <button>Boop</button>;
+  return (
+    <button type="button" className="bg-blue-500 text-rose-500">
+      Boop
+    </button>
+  );
 };
